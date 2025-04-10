eeaf34db30204043b94d74cbaf509eba2c930e4a
chaitanyantr <chaitanyantr@gmail.com>
2025-04-10 11:55:47 -0700

    bi manual launch files

--- plexus/ros2/plexus_pkg/setup.py
+++ plexus/ros2/plexus_pkg/setup.py
@@ -18,7 +18,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/allegro_hand_launch.py', 'launch/sensor_patch_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/allegro_hand_launch.py', 'launch/sensor_patch_launch.py', 'launch/tmr_hand_launch.py',]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
