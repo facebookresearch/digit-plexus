d19b7f3d44c44ae672701d84f738cc1b9796e34f
Mike Lambeta <beta@alphabeta.ca>
2025-04-23 20:10:53 -0700

    update dual hand example and launch by serial

--- plexus/ros2/plexus_pkg/launch/dual_hand_launch.py
+++ plexus/ros2/plexus_pkg/launch/dual_hand_launch.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 
@@ -22,27 +21,48 @@ from plexus.ros2.plexus_pkg.plexus_pkg.allegro_driver import AllegroDriver
 import can
 from typing import Dict
 
+# Edit this to map the allegro hand serial to either a left or right hand
+HAND_MAPPING_CONFIG = {
+    "left": "T1234".lower(),
+    "right": "T2345".lower(),
+}
+
+
 def get_allegro_configs() -> Dict[str, Path]:
     launch_root = os.path.dirname(os.path.abspath(__file__))
-    interfaces=can.detect_available_configs(interfaces="socketcan")
+    interfaces = can.detect_available_configs(interfaces="socketcan")
     configs = {}
+
     for iface in interfaces:
         allegro_driver = AllegroDriver(can_channel=iface["channel"])
         try:
             allegro_serial = allegro_driver.get_serial().lower()
-            print("read serial from ", iface, allegro_serial)
+            print("Read serial from ", iface, allegro_serial)
         except TimeoutError:
-            print('failed to read serial from ', iface)
+            print("Failed to read serial from ", iface)
             continue
-        allegro_config_serial = Path(f"{launch_root}/allegro_node_params_{allegro_serial}.yaml")
-        allegro_config_default= Path(f"{launch_root}/allegro_node_params.yaml")
-    
-        if allegro_config_serial.exists():
-            print(f'using hand specific config - {allegro_config_serial.absolute()}')
-            configs[iface["channel"]] = allegro_config_serial
-        else:
-            print(f'using default config - {allegro_config_default.absolute()}')
-            configs[iface["channel"]] = allegro_config_default
+
+        matched_hand_serial = None
+        for hand, serial in HAND_MAPPING_CONFIG.items():
+            if allegro_serial == serial:
+                matched_hand_serial = hand
+                break
+
+        if not matched_hand_serial:
+            raise ValueError(f"Unknown Allegro Hand serial number: {allegro_serial}")
+
+        allegro_config_serial = Path(
+            f"{launch_root}/allegro_node_params_{allegro_serial}.yaml"
+        )
+        allegro_config_default = Path(f"{launch_root}/allegro_node_params.yaml")
+
+        config_path = (
+            allegro_config_serial
+            if allegro_config_serial.exists()
+            else allegro_config_default
+        )
+        print(f"Using config for {matched_hand_serial} - {config_path.absolute()}")
+        configs[iface["channel"]] = (config_path, matched_hand_serial)
 
     return configs
 
@@ -50,25 +70,21 @@ def get_allegro_configs() -> Dict[str, Path]:
 def _generate_launch_description(context):
     configs = get_allegro_configs()
     nodes = []
-    for can_interface, config in configs.items():
-        if can_interface == "can0":
-            _ns = "right_hand"
-        elif can_interface == "can2":
-            _ns = "left_hand"
-        else:
-            _ns = f"allegroHand_{can_interface}"
 
-        allegro_controller_node  = Node(
+    for can_interface, (config, hand) in configs.items():
+        _ns = f"{hand}_hand"
+
+        allegro_controller_node = Node(
             namespace=_ns,
             package="plexus_pkg",
             executable="allegro_controller_node.py",
             name="allegro_controller",
             parameters=[config, {"can_interface": can_interface}],
-            respawn=True
+            respawn=True,
         )
         nodes.append(allegro_controller_node)
 
-        allegro_info_node  = Node(
+        allegro_info_node = Node(
             namespace=_ns,
             package="plexus_pkg",
             executable="allegro_info_node.py",
@@ -77,19 +93,18 @@ def _generate_launch_description(context):
         )
         nodes.append(allegro_info_node)
 
-
         shutdown_action = RegisterEventHandler(
             OnProcessExit(
                 target_action=allegro_controller_node,
                 on_exit=[
-                    LogInfo(msg="critical node failure"),
-                    EmitEvent(event=Shutdown(
-                        reason='critical node failure'))
-                ]
+                    LogInfo(msg="Plexus critical node failure"),
+                    EmitEvent(event=Shutdown(reason="Plexus critical node failure")),
+                ],
             )
         )
         nodes.append(shutdown_action)
     return nodes
+
 
 def generate_launch_description():
     return LaunchDescription([OpaqueFunction(function=_generate_launch_description)])
